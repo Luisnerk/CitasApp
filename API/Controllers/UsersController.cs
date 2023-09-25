496e906ad21270ac7037fc313d9caa419bc1cbc4
Luisnerk <luis_calix@live.com>
2023-09-25 07:25:16 -0600

    Sección  - Added Identity Service

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,10 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 [ApiController]
 [Route("api/[controller]")] // api/users
+
+[Authorize]
 public class UsersController : ControllerBase
 {
     private readonly DataContext _context;
@@ -14,12 +17,14 @@ public class UsersController : ControllerBase
         _context = context;
     }
 
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
         return await _context.Users.ToListAsync();
     }
 
+    [Authorize]
     [HttpGet("{id}")]
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
