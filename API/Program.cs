9c2ce556a75ad84572291592fbb3bdc0eac85d3b
Luisnerk <luis_calix@live.com>
2023-11-06 07:32:13 -0600

    Seccion08 - Manejo EF

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,7 @@
-using System.Text;
+using API.Data;
 using API.Extensions;
 using API.Middleware;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,11 +21,24 @@ app.UseCors(builder => builder.AllowAnyHeader()
                                 .WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "Ha ocurrido un error durante la el sembrado");
+}
+
 
 app.Run();
