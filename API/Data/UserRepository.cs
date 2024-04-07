bc773e88edb01d1747aaf50bd2532e9e2337c274
Luisnerk <luis_calix@live.com>
2024-04-07 15:34:14 -0600

    Parcial02 - Added sorting to members API

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -33,11 +33,21 @@ public class UserRepository : IUserRepository
         //     .ToListAsync();
         
         var query = _context.Users.AsQueryable();
+
         query = query.Where(u => u.UserName != userParams.CurrentUsername);
         query = query.Where(u => u.Gender == userParams.Gender);
+
         var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
         var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+        
         query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob); 
+
+        query = userParams.OrderBy switch
+        {
+            "created" => query.OrderByDescending(u => u.Created),
+            "dob" => query.OrderByDescending(u => u.DateOfBirth),
+            _ => query.OrderByDescending(u => u.LastActive)
+        };
 
         return await PagedList<MemberDto>.CreateAsync(query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider), userParams.PageNumber, userParams.PageSize);
     }
