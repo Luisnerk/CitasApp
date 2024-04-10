413c683bd89ccc3ac0828c76b05443052e5ce5f4
Luisnerk <luis_calix@live.com>
2024-04-09 22:24:48 -0600

    Added caching of member

--- SPA/src/app/_services/members.service.ts
+++ SPA/src/app/_services/members.service.ts
@@ -12,10 +12,17 @@ import { UserParams } from '../_models/userParams';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map();
  
   constructor(private http: HttpClient) { }
  
   getMembers(userParams: UserParams): Observable<PaginatedResult<Member[]>> {
+    const response = this.memberCache.get(Object.values(userParams).join("-"));
+
+    if (response) {
+      return of(response);
+    }
+
     let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
 
     params = params.append("minAge", userParams.minAge);
@@ -23,9 +30,43 @@ export class MembersService {
     params = params.append("gender", userParams.gender);
     params = params.append("orderBy", userParams.orderBy);
 
-    return this.getPaginatedResult<Member[]>(this.baseUrl + "users", params);
+    return this.getPaginatedResult<Member[]>(this.baseUrl + "users", params).pipe(
+      map(response => {
+        this.memberCache.set(Object.values(userParams).join("-"), response);
+        return response;
+      })
+    
+    );
   }
  
+  getMember(username: string): Observable<Member> {
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((member: Member) => member.userName === username);
+
+    if (member) return of(member);
+
+    return this.http.get<Member>(this.baseUrl + "users/" + username);
+  }
+ 
+  updateMember(member: Member): Observable<void> {
+    return this.http.put(this.baseUrl + "users", member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = { ...this.members[index], ...member };
+      }),
+    );
+  }
+ 
+  setMainPhoto(photoId: number): Observable<Object> {
+    return this.http.put(this.baseUrl + "users/photo/" + photoId, {});
+  }
+ 
+  deletePhoto(photoId: number): Observable<Object> {
+    console.log("photoId de nuevo: " + photoId);
+    return this.http.delete(this.baseUrl + "users/photo/" + photoId, {});
+  }
+  
   private getPaginatedResult<T>(url: string, params: HttpParams): Observable<PaginatedResult<T>> {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>;
     return this.http.get<T>(url, { observe: "response", params }).pipe(
@@ -47,29 +88,5 @@ export class MembersService {
     params = params.append("pageNumber", pageNumber);
     params = params.append("pageSize", pageSize);
     return params;
-  }
- 
-  getMember(username: string): Observable<Member> {
-    const member = this.members.find(x => x.userName === username);
-    if (member) return of(member);
-    return this.http.get<Member>(this.baseUrl + "users/" + username);
-  }
- 
-  updateMember(member: Member): Observable<void> {
-    return this.http.put(this.baseUrl + "users", member).pipe(
-      map(() => {
-        const index = this.members.indexOf(member);
-        this.members[index] = { ...this.members[index], ...member };
-      }),
-    );
-  }
- 
-  setMainPhoto(photoId: number): Observable<Object> {
-    return this.http.put(this.baseUrl + "users/photo/" + photoId, {});
-  }
- 
-  deletePhoto(photoId: number): Observable<Object> {
-    console.log("photoId de nuevo: " + photoId);
-    return this.http.delete(this.baseUrl + "users/photo/" + photoId, {});
   }
 }
