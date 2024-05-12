4211c9e7605ea1c97106fdf8e780ca46e554a17c
Luisnerk <luis_calix@live.com>
2024-05-12 13:32:57 -0600

    Parcial03 - Added pagination to likes response

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,3 @@
-using System.Security.Claims;
-using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
@@ -8,7 +6,6 @@ using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -16,11 +13,14 @@ namespace API.Controllers;
 public class UsersController : BaseApiController
 {
     #region Private vars and ctor
+
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
     private readonly IPhotoService _photoService;
 
-    public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
+
+    public UsersController(IUserRepository userRepository, IMapper mapper,
+        IPhotoService photoService)
     {
         _userRepository = userRepository;
         _mapper = mapper;
@@ -30,22 +30,26 @@ public class UsersController : BaseApiController
     #endregion
 
     [HttpGet]
-    public async Task<ActionResult<PagedList<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
+    public async Task<ActionResult<PagedList<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
     {
-        var currentUser = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+        var currentUser = await _userRepository
+            .GetUserByUsernameAsync(User.GetUsername());
         userParams.CurrentUsername = currentUser.UserName;
+
+        if (string.IsNullOrEmpty(userParams.Gender))
+        {
+            userParams.Gender = currentUser.Gender == "female" ?
+                "male" : "female";
+        }
+        
         var users = await _userRepository.GetMembersAsync(userParams);
 
-        if (string.IsNullOrEmpty(userParams.Gender)) {
-            userParams.Gender = currentUser.Gender == "female" ? "male" : "female";
-        }
-
-        users.TotalCount = 10;
-        Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages));
+        Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize,
+            users.TotalCount, users.TotalPages));
 
         return Ok(users);
     }
-    
+
     [HttpGet("{username}")]
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
@@ -56,14 +60,14 @@ public class UsersController : BaseApiController
     public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
     {
         var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
-        
+
         if (user == null) return NotFound();
 
         _mapper.Map(memberUpdateDto, user);
 
         if (await _userRepository.SaveAllAsync()) return NoContent();
 
-        return BadRequest("Failed to update user");
+        return BadRequest("No se pudo realizar la operación");
     }
 
     [HttpPost("photo")]
@@ -87,62 +91,54 @@ public class UsersController : BaseApiController
 
         user.Photos.Add(photo);
 
-        if (await _userRepository.SaveAllAsync()){
-            // return _mapper.Map<PhotoDto>(photo);
-            return CreatedAtAction(nameof(GetUser), 
-                new {username = user.UserName},
+        if (await _userRepository.SaveAllAsync())
+        {
+            return CreatedAtAction(
+                nameof(GetUser),
+                new { username = user.UserName },
                 _mapper.Map<PhotoDto>(photo));
         }
 
         return BadRequest("Hubo un problema al subir tu foto");
     }
 
-    [HttpPut("Photo/{photoId}")]
+    [HttpPut("photo/{photoId}")]
     public async Task<ActionResult> SetMainPhoto(int photoId)
     {
         var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+        if (user == null) return NotFound("No se encuentra el usuario");
 
-        if (user == null) return NotFound();
+        var newMain = user.Photos.FirstOrDefault(photo => photo.Id == photoId);
+        if (newMain == null) return NotFound("No se encuentra la foto");
+        if (newMain.IsMain) return BadRequest("La foto ya es la principal");
 
-        var photo = user.Photos.FirstOrDefault(photo => photo.Id == photoId);
-
-        if (photo == null) return NotFound();
-
-        if (photo.IsMain) return BadRequest("Esta ya es tu foto principal");
-
-        var currentMainPhoto = user.Photos.FirstOrDefault(photo => photo.IsMain);
-
-        if (currentMainPhoto != null) currentMainPhoto.IsMain = false;
-
-        photo.IsMain = true;
+        var currentMain = user.Photos.FirstOrDefault(photo => photo.IsMain);
+        if (currentMain != null) currentMain.IsMain = false;
+        newMain.IsMain = true;
 
         if (await _userRepository.SaveAllAsync()) return NoContent();
 
-        return BadRequest("No se pudo establecer tu foto como principal");
+        return BadRequest("No se pudo establecer la foto como principal");
     }
 
     [HttpDelete("photo/{photoId}")]
     public async Task<ActionResult> DeletePhoto(int photoId)
     {
         var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
-
-        var photo = user.Photos.FirstOrDefault(photo => photo.Id == photoId);
+        var photo = user.Photos.FirstOrDefault(p => p.Id == photoId);
 
         if (photo == null) return NotFound();
-
         if (photo.IsMain) return BadRequest("No se puede borrar la foto principal");
-
         if (photo.PublicId != null)
         {
             var result = await _photoService.DeletePhotoAsync(photo.PublicId);
-
             if (result.Error != null) return BadRequest(result.Error.Message);
         }
 
         user.Photos.Remove(photo);
 
         if (await _userRepository.SaveAllAsync()) return Ok();
+        return BadRequest("No ha sido posible borrar la foto");
 
-        return BadRequest("No hasido posible borrar la foto");
     }
 }
--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -1,11 +1,12 @@
-
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 
 namespace API.Interfaces;
+
 public interface ILikesRepository
 {
     Task<UserLike> GetUserLikeAsync(int sourceUserId, int targetUserId);
     Task<AppUser> GetUserWithLikesAsync(int userId);
-    Task<IEnumerable<LikeDto>> GetUserLikesAsync(string predicate, int userId);
+    Task<PagedList<LikeDto>> GetUserLikesAsync(LikesParams likesParams);
 }
