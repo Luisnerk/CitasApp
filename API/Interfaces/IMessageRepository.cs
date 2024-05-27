227d27bdb5a77300be4dd32754656a152a4d90c7
Luisnerk <luis_calix@live.com>
2024-05-27 00:42:36 -0600

    Parcial03 - Created messages controller, updated message repository and added message params

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -1,14 +1,15 @@
-using API.DTOs;
 using API.Entities;
 using API.Helpers;
+using API.DTOs;
 
 namespace API.Interfaces;
+
 public interface IMessageRepository
 {
-    void AddMessage (Message message);
-    void DeleteMessage (Message message);
-    Task<Message> GetMessageAsync (int id);
-    Task<PagedList<MessageDto>> GetMessagesForUserAsync ();
-    Task<IEnumerable<MessageDto>> GetMessageThreadAsync (int currentUserId, int recipientId);
-    Task<bool> SaveAllAsync ();
+    void AddMessage(Message message);
+    void DeleteMessage(Message message);
+    Task<Message> GetMessageAsync(int id);
+    Task<PagedList<MessageDto>> GetMessagesForUserAsync(MessageParams messageParams);
+    Task<IEnumerable<MessageDto>> GetMessageThreadAsync(string currentUserName, string recipientUserName);
+    Task<bool> SaveAllAsync();
 }
